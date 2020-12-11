3bf2a12db12c563329ea5640243b70745653538b
Michael Gutierrez <mwgutier@gmail.com>
2020-12-11 15:13:21 -0700

    Fixing site

--- ssg/site.py
+++ ssg/site.py
@@ -1,11 +1,13 @@
-import path from pathlib
+from pathlib import Path
+
 
 class Site:
     def __init__(self, source, dest):
-        self.path = path(source)
+        self.source = Path(source)
+        self.dest = Path(dest)
 
     def create_dir(self, path):
-        self.directory = self.dest / self.path.relative_to(self.source)
+        self.directory = self.dest / path.relative_to(self.source)
         path.mkdir(self.directory, parents=True,exists_ok=True)
 
     def build(self):
